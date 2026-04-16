cd8a62bad35b3dc033561ddc603e3bd480bc3f65
tititinnn <tinnguyenhuu4@gmail.com>
2026-04-17 00:29:02 +0700

    thêm trang loading (là main) -> login -> home

--- app/src/main/java/com/example/btl_novelworld/MainActivity.java
+++ app/src/main/java/com/example/btl_novelworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.btl_novelworld;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,28 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        tvUserEmail = findViewById(R.id.tvUserEmail);
-        btnLogout = findViewById(R.id.btnLogout);
-
-        // Lấy thông tin user hiện tại
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) {
-            tvUserEmail.setText("Tài khoản: " + user.getEmail());
-        } else {
-            // Nếu chưa đăng nhập mà lọt vào đây thì đá về trang Login
-            backToLogin();
-        }
-
-        // Xử lý Đăng xuất
-        btnLogout.setOnClickListener(v -> {
-            mAuth.signOut(); // Xóa phiên đăng nhập trên Firebase
-            backToLogin();
-        });
-    }
-
-    private void backToLogin() {
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        finish();
+        // Đợi 2.5 giây rồi chuyển sang trang chủ
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Chuyển sang MainActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Kết thúc màn hình loading
+            }
+        }, 2500);
     }
 }
